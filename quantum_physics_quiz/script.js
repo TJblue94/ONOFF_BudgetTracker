4cf6469589103e253dd908808cf2f625ba510b70
Tim Bluhm <tjbluhm00@gmail.com>
2020-09-29 23:38:07 -0500

    hw4

---
+++ quantum_physics_quiz/script.js
@@ -0,0 +1,11 @@
+var startButton = document.getElementById('start')
+var qQuestions = document.getElementById()
+var qAnswers = document.getElementByclass(btn)
+
+
+
+startButton.addEventListener('click', showResults);
+
+array.forEach(element => {
+    
+});
